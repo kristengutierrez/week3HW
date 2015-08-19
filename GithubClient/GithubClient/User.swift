0b7b88c0077dbbd9ce886b2c1876a34b0a3543ee
kristenkozmary <kristenkozmary@yahoo.com>
2015-08-19 15:49:12 -0700

    m

---
+++ GithubClient/GithubClient/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  GithubClient
+//
+//  Created by Kristen Kozmary on 8/19/15.
+//  Copyright (c) 2015 koz. All rights reserved.
+//
+
+import Foundation
+
+
+struct User {
+  let login : String
+  let avatarURL : String
+  let id : String
+}
